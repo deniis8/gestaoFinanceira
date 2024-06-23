7afafe57e5296e6dd8af7221c04ace8ddd497cb9
Adenilson Soares <adenilson_denis8@hotmail.com>
2024-06-23 01:29:27 -0300

    Ajustes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { Lancamento } from 'src/app/Lancamento';
 import { LancamentoService } from 'src/app/services/lancamento.service';
 
@@ -9,6 +9,8 @@ import { LancamentoService } from 'src/app/services/lancamento.service';
 })
 export class HomeComponent {
   lancamentos: Lancamento[] = [];
+  dataDe: string = "";
+  dataAte: string = "";
 
   constructor(private lancamentoService: LancamentoService) {
   }
@@ -17,8 +19,22 @@ export class HomeComponent {
     this.lancamentoService.getAllLancamentos().subscribe((lancamentos) => (this.lancamentos = lancamentos));
   }
 
+  filtroData(): void {
+    if(this.dataDe != "" && this.dataAte != ""){
+      this.lancamentoService.getLancamentoDataDeAte(this.dataDe, this.dataAte).subscribe(item => {
+        let infoLancamentos = item;
+        this.lancamentos = [];
+        if (infoLancamentos != null) {
+          for (let i = 0; i < infoLancamentos.length; i++) {        
+            this.lancamentos.push(infoLancamentos[i])
+          }       
+        }
+      });
+    }    
+  }
+
   removerLancamento(id: Number): void {
-    console.log("Id: " + id)
+    //console.log("Id: " + id)
     this.lancamentoService.excluirLancamento(id).subscribe();
   }
 }
